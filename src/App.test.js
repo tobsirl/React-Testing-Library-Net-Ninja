2a585bd25eb33dbe6b5f5c58a7edb8e514521019
Paul Tobin <tobster111@gmail.com>
2021-08-02 17:15:10 +0100

    Test for header component working

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Header from './components/Header/Header';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  render(<Header title="Todo" />);
+
+  const linkElement = screen.getByText(/todo/i);
+
   expect(linkElement).toBeInTheDocument();
 });
