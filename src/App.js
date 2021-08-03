920390416d395d6981763d9e5c8cdb9e4cc0dadb
Paul Tobin <tobster111@gmail.com>
2021-08-03 18:21:06 +0100

    Removed logo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
 import Banner from './components/Banner/Banner';
-import { Switch, Route } from "react-router-dom"
+import { Switch, Route } from 'react-router-dom';
 import TodoPage from './pages/TodoPage/TodoPage';
 import FollowersPage from './pages/FollowersPage/FollowersPage';
 
@@ -10,8 +9,8 @@ function App() {
     <div className="App">
       <Banner />
       <Switch>
-        <Route strict exact path="/" component={TodoPage}/>
-        <Route strict exact path="/followers" component={FollowersPage}/>
+        <Route strict exact path="/" component={TodoPage} />
+        <Route strict exact path="/followers" component={FollowersPage} />
       </Switch>
     </div>
   );
